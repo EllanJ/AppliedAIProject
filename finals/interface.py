fa2f619f7533bdf6ebf6aa27404ba84d8fd3b48c
Rey Anthony Novero <2woaranthony@gmail.com>
2024-05-28 00:38:54 +0800

    that's all, the end.

--- finals/interface.py
+++ finals/interface.py
@@ -13,13 +13,13 @@ import google.generativeai as genai
 
 
 recognizer = sr.Recognizer()
-os.environ['GOOGLE_API_KEY'] = 'yourapikey'
+os.environ['GOOGLE_API_KEY'] = 'insert api key here'
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 generation_config = {
     "temperature": 0.7,
     "top_p": 1,
     "top_k": 1,
-    "max_output_tokens": 80
+    "max_output_tokens": 100
 }
 
 def get_voice_input():
@@ -53,18 +53,18 @@ def weather(command):
         sky = data[1]
         response = f"It is currently {temp} and {sky} in {region}"
         talk(response)
-        st.write(f"Arthur's Response: {response}")
+        st.write(f"Arthuria's Response: {response}")
     except Exception as e:
         error_message = "Sorry, I couldn't fetch the weather information."
         st.write(f"Error: {e}")
         talk(error_message)
-        st.write(f"Arthur's Response: {error_message}")
+        st.write(f"Arthuria's Response: {error_message}")
 
 def open_application(command):
     app_opened = False
     if 'chrome' in command:
         talk("Opening Google Chrome")
-        subprocess.Popen('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Google Chrome.exe')
+        subprocess.Popen(['open', '-a', 'Google Chrome'])
         app_opened = True
     elif 'notepad' in command:
         talk("Opening Notepad")
@@ -76,20 +76,57 @@ def open_application(command):
         app_opened = True
     elif 'word' in command:
         talk("Opening Microsoft Word")
-        subprocess.Popen('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Word.exe')
+        subprocess.Popen(['open', '-a', 'Microsoft Word'])
         app_opened = True
     elif 'excel' in command:
         talk("Opening Microsoft Excel")
-        subprocess.Popen('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\Excel.exe')
+        subprocess.Popen(['open', '-a', 'Microsoft Excel'])
         app_opened = True
     elif 'powerpoint' in command:
         talk("Opening Microsoft PowerPoint")
-        subprocess.Popen('C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\PowerPoint.exe')
+        subprocess.Popen(['open', '-a', 'Microsoft PowerPoint'])
+        app_opened = True
+    elif 'calculator' in command:
+        talk("Opening Calculator")
+        subprocess.Popen(['open', '-a', 'Calculator'])
         app_opened = True
     if not app_opened:
         response = "Sorry, I am not able to open that application."
         talk(response)
-        st.write(f"Arthur's Response: {response}")
+        st.write(f"Arthuria's Response: {response}")
+
+    # windows only
+    # def open_application(command):
+    #     app_opened = False
+    #     if 'chrome' in command:
+    #         talk("Opening Google Chrome")
+    #         subprocess.Popen('C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe')
+    #         app_opened = True
+    #     elif 'notepad' in command:
+    #         talk("Opening Notepad")
+    #         subprocess.Popen('notepad.exe')
+    #         app_opened = True
+    #     elif 'paint' in command:
+    #         talk("Opening Paint")
+    #         subprocess.Popen('mspaint.exe')
+    #         app_opened = True
+    #
+    #     elif 'word' in command:
+    #         talk("Opening Microsoft Word")
+    #         subprocess.Popen('C:\\Program Files\\Microsoft Office\\root\\Office16\\WINWORD.exe')
+    #         app_opened = True
+    #     elif 'excel' in command:
+    #         talk("Opening Microsoft Excel")
+    #         subprocess.Popen('C:\\Program Files\\Microsoft Office\\root\\Office16\\EXCEL.exe')
+    #         app_opened = True
+    #     elif 'powerpoint' in command:
+    #         talk("Opening Microsoft PowerPoint")
+    #         subprocess.Popen('C:\\Program Files\\Microsoft Office\\root\\Office16\\POWERPNT.exe')
+    #         app_opened = True
+    #     if not app_opened:
+    #         response = "Sorry, I am not able to open that application."
+    #         talk(response)
+    #         st.write(f"Arthur's Response: {response}")
 
 def process_user_input(user_input):
     command = user_input.lower()
@@ -98,35 +135,36 @@ def process_user_input(user_input):
     if 'play' in command:
         song = command.replace('play', '').strip()
         response = 'playing now'
-        talk(response)
+
         pywhatkit.playonyt(song)
-        st.write(f"Arthur's Response: {response}")
+        st.write(f"Arthuria's Response: {response}")
         st.write('Playing now...')
+        talk(response)
 
     elif 'time' in command:
         time_now = datetime.datetime.now().strftime('%I:%M %p')
         response = 'Time Check, it is ' + time_now
+        st.write(f"Arthuria's Response: {response}")
         talk(response)
-        st.write(f"Arthur's Response: {response}")
     elif 'date' in command:
         current_date = datetime.datetime.now().strftime('%B %d, %Y')
         response = 'The date today is ' + current_date
+        st.write(f"Arthuria's Response: {response}")
         talk(response)
-        st.write(f"Arthur's Response: {response}")
     elif 'what' in command:
         info = command.replace('what', '').strip()
         result = wikipedia.summary(info, sentences=1)
         talk(result)
-        st.write(f"Arthur's Response: {result}")
+        st.write(f"Arthuria's Response: {result}")
     elif 'who' in command:
         info = command.replace('who', '').strip()
         result = wikipedia.summary(info, sentences=1)
         talk(result)
-        st.write(f"Arthur's Response: {result}")
+        st.write(f"Arthuria's Response: {result}")
     elif 'joke' in command:
         joke = pyjokes.get_joke()
         talk(joke)
-        st.write(f"Arthur's Response: {joke}")
+        st.write(f"Arthuria's Response: {joke}")
     elif 'open' in command:
         open_application(command)
     elif 'weather' in command:
@@ -135,15 +173,17 @@ def process_user_input(user_input):
         model = genai.GenerativeModel("gemini-1.5-flash", generation_config=generation_config)
         response = model.generate_content([user_input])
         for chunk in response:
-            st.write(f"Arthur's Response: {chunk.text}")
+            st.write(f"Arthuria's Response: {chunk.text}")
             talk(chunk.text)
 
-def main():
-    st.title("Arthur Voice Assistant")
-    st.markdown("---")
-    st.write("Press Activate Arthur to start.")
 
-    if st.button("Activate Arthur"):
+
+def main():
+    st.title("Arthuria Voice Assistant")
+    st.markdown("---")
+    st.write("Press Activate Arthuria to start.")
+
+    if st.button("Activate Arthuria"):
         st.write("Listening...")
         recognized_text = get_voice_input()
         if recognized_text:
